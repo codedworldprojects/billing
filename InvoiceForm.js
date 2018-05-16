93cefe2046a5d547a94a38ad4ea0cf855d5cb6f5
MishraShubhanshu <34263650+MishraShubhanshu@users.noreply.github.com>
2018-05-16 20:47:28 +0530

    Add Functionality Implemented

--- InvoiceForm.js
+++ InvoiceForm.js
@@ -1,9 +1,12 @@
 var invoiceForm = angular.module("InvoiceForm", []);
 
 invoiceForm.controller("InvoiceCtrl", ["$scope", function($scope){
+    $scope.model = {};
+    $scope.model.products = [];
 
     var init = function(){
         $scope.data=  [{"agence":"CTM","secteur":"Safi","statutImp":"operationnel"}];
+        $scope.addProduct();
     };
 
     $scope.exportToPDF = function(){
@@ -18,7 +21,26 @@ invoiceForm.controller("InvoiceCtrl", ["$scope", function($scope){
         html2pdf(html, pdf, function(pdf) {  
             pdf.save('RequisationForm.pdf');  
         }); 
-    }
+    };
+
+    $scope.addProduct = function(){
+        var product = {
+            productCode: "",
+            productName: "",
+            hsnCode: "",
+            quantity: "",
+            rate: "",
+            discount: "",
+            volume: "",
+            volumeType: ""
+        };
+        $scope.model.products.push(product);
+    };
+
+    $scope.removeProduct = function(product){
+        var index = $scope.model.products.indexOf(product);
+        $scope.model.products.splice(index, 1); 
+    };
 
     init();
 }]);
